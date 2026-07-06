a902606e92810e615fab25d98214dd6d1109ee19
abhaylokare1 <abhaylokare123@gmail.com>
2026-07-06 13:16:26 +0530

    Changed css styles optimization

--- script.js
+++ script.js
@@ -328,3 +328,32 @@ console.log(
   "%cDesigned and Developed by Abhay Lokare",
   "font-size:16px;color:#ffffff;"
 );
+const menuBtn = document.querySelector(".menu-btn");
+const mobileMenu = document.querySelector(".nav-links");
+const menuIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click",()=>{
+
+  navLinks.classList.toggle("active");
+
+  if(navLinks.classList.contains("active")){
+    menuIcon.classList.remove("fa-bars");
+    menuIcon.classList.add("fa-xmark");
+  }else{
+    menuIcon.classList.remove("fa-xmark");
+    menuIcon.classList.add("fa-bars");
+  }
+
+});
+
+document.querySelectorAll(".nav-links a").forEach(link=>{
+
+  link.addEventListener("click",()=>{
+
+    navLinks.classList.remove("active");
+    menuIcon.classList.remove("fa-xmark");
+    menuIcon.classList.add("fa-bars");
+
+  });
+
+});
